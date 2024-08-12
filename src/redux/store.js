3d70b12c01b35a0e1430ea7ecab0ee85c5d994c0
sairam0424 <uggesairam0000@gmail.com>
2024-08-12 14:54:57 +0530

    added react-dev-tool logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 
-import { legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import rootReducer from './rootReducer';
 //importing a default export
+import logger  from 'redux-logger';
 
-const store = createStore(rootReducer)
+import { composeWithDevTools } from '@redux-devtools/extension/lib/types/logOnly';
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger)))
 export default store
